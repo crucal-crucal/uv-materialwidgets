b9b4752c0abb46897820fb774359672b036815d3
crucal <s18908155529@163.com>
2024-06-19 20:41:20 +0800

    修正加载字体方式，修复未安装字体机器显示错误问题

--- src/examples/main.cc
+++ src/examples/main.cc
@@ -6,7 +6,17 @@
 
 #include "mainwidget.hpp"
 
+/**
+ * 加载 rcc
+ * @param strPath rcc 文件路径
+ * @return true 加载成功 false 加载失败
+ */
 bool loadResources(const QString& strPath);
+/**
+ * 卸载 rcc
+ * @param strPath rcc 文件路径
+ * @return true 卸载成功 false 卸载失败
+ */
 bool unloadResources(const QString& strPath);
 
 int main(int argc, char* argv[]) {
@@ -22,12 +32,18 @@ int main(int argc, char* argv[]) {
 	// 将路径切换到上级目录
 	const QString appParPath = appFile.absolutePath();
 	const QString strRcc = appParPath + "/resources.rcc";
+
 	if (loadResources(strRcc)) {
 		qInfo() << "load resources success";
 	} else {
 		qInfo() << "load resources failed";
 	}
-	QFontDatabase::addApplicationFont(":/fonts/Awesome/CUVAwesome.ttf");
+
+	if (QFontDatabase::addApplicationFont(":/fonts/cuvawesome") != -1) {
+		qInfo() << "add font success";
+	} else {
+		qWarning() << "add font failed";
+	}
 
 	MainWidget mainWidget;
 	QObject::connect(&mainWidget, &MainWidget::destroyed, [&]() {
