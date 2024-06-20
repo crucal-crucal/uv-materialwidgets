1d0f23864526ec17fcaa5f929fdec8fdd4ae1625
curcal <S18908155520@163.com>
2024-06-20 10:42:53 +0800

    Modified the naming rule and compatible with Ubuntu 22.04 - replenishment

--- setup.cmake
+++ setup.cmake
@@ -2,36 +2,36 @@
 
 # 从环境变量中获取 Qt 路径，查找 qmake
 function(setupQt)
-#    if (WIN32 OR UNIX)
-#        set(QT_DIR $ENV{QTDIR})
-#        message(STATUS "QT_DIR=${QT_DIR}")
-#        find_program(QMAKE_EXECUTABLENAMES qmake HINTS $ENV{QT_DIR} PATH_SUFFIXES bin)
-#        set(CMAKE_PREFIX_PATH ${QT_DIR})
-#    endif ()
-#
-#    # 查询 Qt 版本 并存储再 QT_VERSION 中
-#    execute_process(COMMAND ${QMAKE_EXECUTABLENAMES} -query QT_VERSION OUTPUT_VARIABLE QT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
-#    if (QT_VERSION)
-#        message(STATUS "Qt version: " ${QT_VERSION})
-#        string(REPLACE "." ";" QT_VERSION_LIST ${QT_VERSION})
-#        list(GET QT_VERSION_LIST 0 QT_VERSION_MAJOR)
-#        set(Qt "Qt${QT_VERSION_MAJOR}")
-#    else ()
-#        message(FATAL_ERROR "Qt version not found. Please check if the QTDIR environment variable is set correctly.")
-#    endif ()
-#
-#    include_directories(${QT_DIR}/include)
-#
-#    find_package(${Qt}
-#            COMPONENTS
-#            Core
-#            Gui
-#            Widgets
-#            Svg
-#            Sql
-#            Network
-#            REQUIRED
-#    )
+    if (WIN32 OR UNIX)
+        set(QT_DIR $ENV{QTDIR})
+        message(STATUS "QT_DIR=${QT_DIR}")
+        find_program(QMAKE_EXECUTABLENAMES qmake HINTS $ENV{QT_DIR} PATH_SUFFIXES bin)
+        set(CMAKE_PREFIX_PATH ${QT_DIR})
+    endif ()
+
+    # 查询 Qt 版本 并存储再 QT_VERSION 中
+    execute_process(COMMAND ${QMAKE_EXECUTABLENAMES} -query QT_VERSION OUTPUT_VARIABLE QT_VERSION OUTPUT_STRIP_TRAILING_WHITESPACE)
+    if (QT_VERSION)
+        message(STATUS "Qt version: " ${QT_VERSION})
+        string(REPLACE "." ";" QT_VERSION_LIST ${QT_VERSION})
+        list(GET QT_VERSION_LIST 0 QT_VERSION_MAJOR)
+        set(Qt "Qt${QT_VERSION_MAJOR}")
+    else ()
+        message(FATAL_ERROR "Qt version not found. Please check if the QTDIR environment variable is set correctly.")
+    endif ()
+
+    include_directories(${QT_DIR}/include)
+
+    find_package(${Qt}
+            COMPONENTS
+            Core
+            Gui
+            Widgets
+            Svg
+            Sql
+            Network
+            REQUIRED
+    )
 endfunction()
 
 # 安装 DLL
